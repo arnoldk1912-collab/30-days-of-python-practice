0636ce2e679694e82b21149c4eb1829843a33a1e
Arnold kyro <arnoldk1912@gmail.com>
2026-09-08 02:35:59 +0400

    Day 2: Nested Conditions

--- 30 Days of python/Conditionals 30 days of python.py
+++ 30 Days of python/Conditionals 30 days of python.py
@@ -36,3 +36,55 @@ else:
 
 d = 5 
 print('D is More than') if d > 0 else print('D is less Than')
+
+# Nested Conditions
+
+# Nested conditions Real-world analogy
+# Imagine a bouncer at a club checking people at the door:
+
+   # 1. Outer check: "Are you 18 or older?"
+# If no → rejected immediately. Doesn't matter what else is true about you.
+# If yes → move to the next question (this is where nesting happens)
+
+  # 2.  Inner check (only asked if outer was yes): "Do you have a valid ID?"
+# If yes → let them in
+# If no → rejected, even though they're old enough
+
+a = 0
+if a > 0: #python checks wether a is greater than zero. since a = o, this is a > 0 is false
+    if a % 2 == 0: # it's completely skipped
+        print('A is positive and even integer')
+    else:  # coz the outer if is false, python dosen't even look inside the nested block
+        print('A is a positive number')
+        
+elif a == 0: # Now py checks a ==0. since a actually 0, this is True
+    print('A is Zero')
+    
+else: # This is skipped coz elif matched so py never check the final else chain
+    print('A is a negative number')
+    
+# Nested Conditions workout
+age = int(input('Enter your age: '))
+day = input('Enter day type (weekend/weekday): ')
+
+if age < 12:
+    print('Ticket price: $5')
+else:
+    if day == 'weekend':
+        print('Ticket price: $12')
+    else:
+        print('Ticket price: $8')
+        
+order_total = float(input('Enter order total: '))
+member = input('Are they a member? (Yes/no): ')
+
+if order_total < 20:
+    print('No discount')
+else:
+    if member == 'Yes':
+        print('10% discount applide')
+    else: 
+        print('5% discount applide')
+    
+
+ 
